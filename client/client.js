1d89a159f318e8785bb00b577f59aa050834abf2
thederpykrafter <thederpykrafter@gmail.com>
2024-03-12 22:15:23 +0000

    add character placeholder

--- client/client.js
+++ client/client.js
@@ -14,15 +14,27 @@ socket.on("update", (data) =>{
 
 function draw(data){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
+  // draw backgroumd
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  // draw players
   for (let i = 0; i < data.length; i++) {
-    ctx.fillStyle = "black";
+    let playerSize = 60;
+    let playerRad = playerSize / 2;
+    
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(data[i].x - playerRad, data[i].y - playerRad, playerSize, playerSize);
+
+    ctx.fillStyle="green"
+    ctx.fillRect(data[i].x, data[i].y, 1, 1);;
+
+    ctx.fillStyle = "black"
     ctx.fillText(data[i].number, data[i].x, data[i].y);
   }
 }
+
 function fullscreen() {
   document.documentElement.requestFullscreen().catch(e => {
     console.log(e);
